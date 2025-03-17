f4ceabaad70b05682ab78a8083633b63e936fc00
JuanMa-maker <jsanchezvalaz@gmail.com>
2025-03-16 19:37:47 -0600

    Update ContentView.swift
    Prueba para pull de Xcode

--- AppPruebasSwiftDesarrollo/ContentView.swift
+++ AppPruebasSwiftDesarrollo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hola, mundo!")
+            Text("Hola, mundo2!")
         }
         .padding()
     }
